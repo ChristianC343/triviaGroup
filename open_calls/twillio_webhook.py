23557bd764da2c2f0023e921bf81e53dc22cc5bf
Ayoub Ibrahim <ayoubibrahim@Ayoubs-MacBook-Pro.local>
2023-02-20 12:12:25 -0800

    made lots of changes twillio, q&a, qanda

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -1,16 +1,34 @@
+
 import yaml
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from os.path import exists
 
 
+
+
 from tools.logging import logger
 from things.actors import actor
+
 
 
 import random
 import json
 import pickle
+import qanda
+
+class actor:
+    def __init__(self, phone_number):
+        self.phone = phone_number
+        self.prev_msgs = []
+        self.state = {"init_flag": True}  # initialize the state dictionary with the "init_flag" variable set to True
+        self.waiting = False
+
+    def save_msg(self, msg):
+        self.prev_msgs.append(msg)
+
+
+
 
 
 yml_configs = {}
@@ -20,53 +38,79 @@ with open('config.yml', 'r') as yml_file:
 
 CORPUS = {}
 TRIVIA = {}
-next_prompt = None
+
 
 with open('chatbot_corpus.json', 'r') as myfile: # open and read the json file
     CORPUS = json.loads(myfile.read())
+with open('chatbot_trivia.json', 'r') as myfile: # open and read the json file
+    TRIVIA = json.loads(myfile.read())
+
+
+def startGame(numOfQues):
+    game = qanda.Qanda()
+    with open("q&a") as file: #open and read q&a file
+            counter = 1
+            for line in file:
+                if line == "":
+                    break
+                elif counter % 2 == 0:  # start at line 1. If line is odd, it is a question. If even, it is an answer
+                    game.alist.append(line)
+                    counter += 1
+                else:
+                    game.qlist.append(line)
+                    counter += 1
+    response = "Let's start the game!\n"
+    for i in range(numOfQues):
+        q = game.qlist[i]
+        a = game.alist[i].split(",")
+        response += f"\nQuestion {i+1}: {q}"
+        response += f"\nA. {a[0]}"
+        response += f"\nB. {a[1]}"
+        response += f"\nC. {a[2]}"
+        response += f"\nD. {a[3]}"
+    response += "\n\nPlease respond with your answers as A, B, C, or D."
+    message = g.sms_client.messages.create(
+        body=response,
+        from_=yml_configs['twillio']['phone_number'],
+        to=request.form['From']
+    )
+    return ""
+
 
 
 def handle_request():
     logger.debug(request.form)
 
-    act = None
-    if exists( f"users/{request.form['From']}.pkl") :
-        with open(f"users/{request.form['From']}.pkl", 'rb') as p:
-            act = pickle.load(p) 
-    else:
-        act= actor(request.form['From'])
+    #act = actor(request.form['From'])
 
-    act.save_msg(request.form['Body'])
-    logger.debug(act.prev_msgs)
+    hello_prompt = 'Hello, Welcome to a Quick Round of Trivia!'
+    select = TRIVIA['selection_prompt']['content']
+    lb = 'Leaderboard'
+    easy = 'Easy Mode'
+    bye = 'Thanks for Playing!'
+
+    sent_input = str(request.form['Body'])
+
+    if sent_input == 'hello': 
+        response = hello_prompt + '\n' + select + '\n'+ '\n'.join(TRIVIA['menu_options'])
+    else:
+        if sent_input in ['1', '2', '3']:
+            if sent_input == '1':
+                response = lb
+            elif sent_input == '2':
+                response = easy
+                startGame(5)
+            elif sent_input == '3':
+                response = bye
+                #progRunning = False
+        else:
+            response = 'Invalid input.'
     
-
-    response = 'NOT FOUND'
-
-    sent_input = str(request.form['Body']).lower()# getting the input sent from the user, converting to lower
-   
-    if sent_input in CORPUS['input']: # check to see the if the sent input is inside the json file
-        response = CORPUS['input'][sent_input]
-    elif sent_input == 'trivia':
-        with open('chatbot_trivia.json', 'r') as myfile:
-            TRIVIA = json.loads(myfile.read())
-            response = TRIVIA['init']['content']
-            next_prompt = TRIVIA['init']['name_prompt']
-    else:
-        CORPUS['input'][sent_input] = ['DID NOT FIND']
-        with open('chatbot_corpus.json', 'w') as myfile:
-            myfile.write(json.dumps(CORPUS, indent=4 ))
+    message = g.sms_client.messages.create(
+            body=response,
+            from_=yml_configs['twillio']['phone_number'],
+            to=request.form['From'])
 
     logger.debug(response)
 
-    message = g.sms_client.messages.create(
-                     body=response,
-                     from_=yml_configs['twillio']['phone_number'],
-                     to=request.form['From'])
-    
-    if next_prompt:
-        message = g.sms_client.messages.create(
-                         body=next_prompt,
-                         from_=yml_configs['twillio']['phone_number'],
-                         to=request.form['From'])
-    
-    return json_response( status = "ok" )
+    return json_response(status="ok")
--- things/actors.py
+++ things/actors.py
@@ -2,7 +2,8 @@ class actor:
     def __init__(self, phone_number):
         self.phone = phone_number
         self.prev_msgs = []
+        self.state = {"init_flag": True}  # initialize the state dictionary with the "init_flag" variable set to True
+        self.waiting = False
 
     def save_msg(self, msg):
         self.prev_msgs.append(msg)
-
