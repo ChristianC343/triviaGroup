22902742c0751e4e444703f6d31e071517fdceec
Adrian4855 <herna552@csusm.edu>
2023-02-27 14:40:37 -0800

    Testing: question counter and excluding repeated questions

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -26,6 +26,57 @@ TRIVIA = {}
 
 with open('chatbot_corpus.json', 'r') as myfile: # open and read the json file
     CORPUS = json.loads(myfile.read())
+with open('chatbot_trivia.json', 'r') as myfile: 
+    TRIVIA = json.loads(myfile.read())
+
+
+import time
+
+
+response = ''
+question_counter = 0
+question_answered = []
+
+
+
+def start_game(user_id, response):
+    questions = TRIVIA['questions']
+    while question_counter < 5:
+        while random_index not in question_answered:
+            random_index = random.randint(0, len(questions)-1)
+            selected_question = questions[random_index]
+
+            options = list(selected_question['options'].values())
+            answer = selected_question['answer']
+            question_text = f"Question : {selected_question['question']}\nA. {options[0]}\nB. {options[1]}\nC. {options[2]}\nD. {options[3]}\nEnter your answer (A, B, C, or D):"
+            # Initialize the user's CORPUS if it doesn't exist
+            if user_id not in CORPUS:
+                CORPUS[user_id] = {}
+
+            # Store the selected question for later reference
+            
+            CORPUS[user_id]['current_question'] = {'question': selected_question['question'], 'answer': answer}
+            
+            with open('chatbot_corpus.json', 'w') as myfile:
+                    myfile.write(json.dumps(CORPUS, indent=4, default=list))
+            message = g.sms_client.messages.create(
+                to=user_id,
+                from_=request.form['To'],
+                body= response 
+            )
+            message = g.sms_client.messages.create(
+                to=user_id,
+                from_=request.form['To'],
+                body= question_text
+            )
+        question_answered.append(random_index)
+        question_counter+=1
+
+    
+
+    #response = ''
+
+
 
 
 def handle_request():
