45f193e43d8499d62b663e96749c2efbb17a33b2
Adrian4855 <herna552@csusm.edu>
2023-02-27 15:05:09 -0800

    Deleted file causing error for me

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -1,8 +1,12 @@
 
+import os
+import time
 import yaml
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from os.path import exists
+
+
 
 
 from tools.logging import logger
@@ -10,9 +14,12 @@ from things.actors import actor
 
 
 
+
 import random
 import json
 import pickle
+import qanda
+
 
 
 yml_configs = {}
@@ -24,7 +31,7 @@ CORPUS = {}
 TRIVIA = {}
 
 
-with open('chatbot_corpus.json', 'r') as myfile: # open and read the json file
+with open('chatbot_corpus.json', 'r') as myfile: 
     CORPUS = json.loads(myfile.read())
 with open('chatbot_trivia.json', 'r') as myfile: 
     TRIVIA = json.loads(myfile.read())
@@ -34,44 +41,37 @@ import time
 
 
 response = ''
-question_counter = 0
-question_answered = []
 
 
 
 def start_game(user_id, response):
     questions = TRIVIA['questions']
-    while question_counter < 5:
-        while random_index not in question_answered:
-            random_index = random.randint(0, len(questions)-1)
-            selected_question = questions[random_index]
+    random_index = random.randint(0, len(questions)-1)
+    selected_question = questions[random_index]
 
-            options = list(selected_question['options'].values())
-            answer = selected_question['answer']
-            question_text = f"Question : {selected_question['question']}\nA. {options[0]}\nB. {options[1]}\nC. {options[2]}\nD. {options[3]}\nEnter your answer (A, B, C, or D):"
-            # Initialize the user's CORPUS if it doesn't exist
-            if user_id not in CORPUS:
-                CORPUS[user_id] = {}
+    options = list(selected_question['options'].values())
+    answer = selected_question['answer']
+    question_text = f"Question : {selected_question['question']}\nA. {options[0]}\nB. {options[1]}\nC. {options[2]}\nD. {options[3]}\nEnter your answer (A, B, C, or D):"
+    # Initialize the user's CORPUS if it doesn't exist
+    if user_id not in CORPUS:
+        CORPUS[user_id] = {}
 
-            # Store the selected question for later reference
-            
-            CORPUS[user_id]['current_question'] = {'question': selected_question['question'], 'answer': answer}
-            
-            with open('chatbot_corpus.json', 'w') as myfile:
-                    myfile.write(json.dumps(CORPUS, indent=4, default=list))
-            message = g.sms_client.messages.create(
-                to=user_id,
-                from_=request.form['To'],
-                body= response 
-            )
-            message = g.sms_client.messages.create(
-                to=user_id,
-                from_=request.form['To'],
-                body= question_text
-            )
-        question_answered.append(random_index)
-        question_counter+=1
-
+    # Store the selected question for later reference
+    
+    CORPUS[user_id]['current_question'] = {'question': selected_question['question'], 'answer': answer}
+    
+    with open('chatbot_corpus.json', 'w') as myfile:
+            myfile.write(json.dumps(CORPUS, indent=4, default=list))
+    message = g.sms_client.messages.create(
+        to=user_id,
+        from_=request.form['To'],
+        body= response 
+    )
+    message = g.sms_client.messages.create(
+        to=user_id,
+        from_=request.form['To'],
+        body= question_text
+    )
     
 
     #response = ''
@@ -82,46 +82,116 @@ def start_game(user_id, response):
 def handle_request():
     logger.debug(request.form)
 
-    #act = actor(request.form['From'])
+    # Load or create user object
+    act = None
+    user_id = request.form['From']
+    filename = f"users/{user_id}.pkl"
+    os.makedirs("users", exist_ok=True)
 
-    act.save_msg(request.form['Body'])
-    logger.debug(act.prev_msgs)
-    
-
-    response = 'NOT FOUND'
-
-    sent_input = str(request.form['Body']).lower()# getting the input sent from the user, converting to lower
-   
-    if sent_input in CORPUS['input']: # check to see the if the sent input is inside the json file
-        response = CORPUS['input'][sent_input]
-    elif sent_input == 'trivia':
-        with open('chatbot_trivia.json', 'r') as myfile:
-            TRIVIA = json.loads(myfile.read())
-            response = TRIVIA['init']['content']
-            next_prompt = TRIVIA['init']['name_prompt']
+    if exists(filename):
+        with open(f"users/{user_id}.pkl", 'rb') as p:
+            act = pickle.load(p)
+        print("I AM CURRENTLY LOADING")
     else:
-        if sent_input in ['1', '2', '3']:
-            if sent_input == '1':
-                response = lb
-            elif sent_input == '2':
-                response = easy
-                startGame(5)
-            elif sent_input == '3':
-                response = bye
-                #progRunning = False
-        else:
-            response = 'Invalid input.'
-    
-    message = g.sms_client.messages.create(
-                     body=response,
-                     from_=yml_configs['twillio']['phone_number'],
-                     to=request.form['From'])
-    
-    if next_prompt:
-        message = g.sms_client.messages.create(
-                         body=next_prompt,
-                         from_=yml_configs['twillio']['phone_number'],
-                         to=request.form['From'])
-    
-    return json_response( status = "ok" )
+        act= actor(request.form['From'])
+        # create the file and write the actor object to it
+        with open(filename, "wb") as f:
+            pickle.dump(actor, f)
+    msg = request.form['Body']
+    print("Printing msg : " + msg)
+    act = actor(user_id)
+    act.save_msg(msg)
+    last_response = act.prev_msgs[-1]
+    logger.debug(act.prev_msgs)
 
+    # Response strings
+    hello_prompt = 'Hello, welcome to a quick round of trivia!'
+    select = TRIVIA['selection_prompt']['content']
+    lb = 'Leaderboard'
+    easy = 'Easy mode'
+    bye = 'Thanks for playing!'
+    invalid = 'Invalid input. Please enter a valid option.'
+
+    # Handle user input
+    input_text = str(request.form['Body']).lower()
+
+    # provide the menu options
+    if last_response == 'Hello':
+        response = hello_prompt + '\n' + select + '\n'+ '\n'.join(TRIVIA['menu_options'])
+        
+   
+    # display the leaderboard
+    elif last_response == '1':
+        leaderboard = act.get_leaderboard()
+        response = f"Leaderboard:\n{leaderboard}"
+    
+    # start a round of easy mode trivia
+    elif last_response == str(2):
+        begin = 'Lets start the game!'
+        # if user_id not in CORPUS:
+        #     CORPUS[user_id] = {}
+        
+        # CORPUS[user_id]['current_game'] = {'question_num' == 0, 'score' == 0}
+        CORPUS[user_id]["current_game"] = {"question_num": 0, "score": 0}
+        # with open('chatbot_corpus.json', 'w') as myfile:
+        #     myfile.write(json.dumps(CORPUS, indent=4, default=list))
+        start_game(user_id, begin)
+    elif last_response == CORPUS[user_id]['current_question']['answer'] :
+        #act.update_score(True)
+        if CORPUS[user_id]['current_game']['question_num'] == 5:
+            CORPUS[user_id]['current_game']['score'] +=1
+            response = 'Correct!' + '\n' + 'Your final score is ' + str(CORPUS[user_id]['current_game']['score']) + '\n' + bye
+        else:
+            CORPUS[user_id]['current_game']['question_num'] +=1 
+            CORPUS[user_id]['current_game']['score'] +=1
+            correct = 'Correct! ' +'\n' + 'Your current score is ' + str(CORPUS[user_id]['current_game']['score']) 
+            start_game(user_id, correct)
+        
+            
+    elif last_response == 'B' or last_response == 'C' or last_response == 'D':
+        #act.update_score(False)
+        
+        if  CORPUS[user_id]['current_game']['question_num'] == 5:
+            response = 'Your final score is ' + str(CORPUS[user_id]['current_game']['score']) + '\n' + bye
+        else:
+            CORPUS[user_id]['current_game']['question_num'] +=1 
+            incorrect = 'Incorrect! ' + '\n' + 'Your current score is ' + str(CORPUS[user_id]['current_game']['score'])
+            start_game(user_id, incorrect)
+            
+    # If the user selects option 3, end the session and save the actor object
+    elif last_response == '3':
+        response = bye
+        with open(f"users/{user_id}.pkl", 'wb') as f:
+            pickle.dump(act, f)
+    
+    # If the user input is invalid, prompt them to enter a valid option
+    else:
+        response = invalid
+        
+    
+    
+    if response:
+        message = g.sms_client.messages.create(
+            body=response,
+            from_=yml_configs['twillio']['phone_number'],
+            to=request.form['From']
+        )
+    else:
+        message = g.sms_client.messages.create(
+            body="Sorry, we encountered an error. Please try again later.",
+            from_=yml_configs['twillio']['phone_number'],
+            to=request.form['From']
+        )
+
+    # Send the response back to the user
+    
+    logger.debug(response)
+    
+    return json_response(status="ok")
+
+
+            
+  
+
+    
+    
--- things/actors.py
+++ things/actors.py
@@ -1,9 +1,19 @@
 class actor:
     def __init__(self, phone_number):
-        self.phone = phone_number
+        self.phone_number = phone_number
+        self.score = 0
         self.prev_msgs = []
-        self.state = {"init_flag": True}  # initialize the state dictionary with the "init_flag" variable set to True
-        self.waiting = False
+        self.num_questions = 0
+
+    def update_score(self, correct):
+        if correct:
+            self.score += 1
+        self.num_questions += 1
 
     def save_msg(self, msg):
         self.prev_msgs.append(msg)
+
+    def reset(self):
+        self.score = 0
+        self.prev_msgs = []
+
